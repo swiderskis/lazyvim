93c80ae2c465fc1073bf5492d1268ae092b7de67
swiderskis <sebastianswiderski42@gmail.com>
2025-10-18 13:46:34 +0100

    Initial commit

---
+++ lua/plugins/config.lua
@@ -0,0 +1,24 @@
+return {
+  { "sainnhe/gruvbox-material" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox-material",
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      diagnostics = {
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = "\u{274c}",
+            [vim.diagnostic.severity.WARN] = "\u{1f4a2}",
+            [vim.diagnostic.severity.HINT] = "\u{1f4a1}",
+            [vim.diagnostic.severity.INFO] = "\u{2139}",
+          },
+        },
+      },
+    },
+  },
+}
